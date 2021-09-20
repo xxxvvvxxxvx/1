f13a0265abf1465c53060892ce1a0315a6111143
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-09-20 06:31:06 +0000

    sus

--- main.py
+++ main.py
@@ -5,5 +5,23 @@ pr
 '''
 print(lama, lama)
 print(lama, end='')
-print(lama, end= '/n/n' )
+print(lama, end='\n\n')
 print(lama.upper())
+print(lama)
+
+lama='tata'
+
+print(lama)
+
+print(lama[0])
+print(lama [1:3])
+
+
+print(lama.upper())
+'''
+susususus
+'''
+
+print(f"my friend is {lama} la")
+a = 'sus'
+print(f"amog {a}")
